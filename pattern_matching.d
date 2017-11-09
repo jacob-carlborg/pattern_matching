4726ab46a1d1069fe90389a9f9bf988fb37190f4
Jacob Carlborg <doob@me.com>
2017-11-09 21:18:36 +0100

    Start adding example

--- pattern_matching.d
+++ pattern_matching.d
@@ -2,7 +2,26 @@ module pattern_matching;
 
 import std.stdio : println = writeln;
 
+auto match(patterns ...)()
+{
+    import std.traits : isType;
+
+    static assert(patterns.length >= 1, "Missing value for match");
+    static assert(patterns.length >= 2, "At least one pattern must be " ~
+        "supplied to match");
+
+    enum paramIsType = isType!(patterns[0]);
+}
+
 void main()
 {
-    println("asd");
+    match!(3,
+        (x) => println("variable pattern"),
+        (int x) => println("typed pattern"),
+        (int) => println("typed pattern, non capturing"),
+        int, (x) => println("typed pattern, second form"),
+        int, () => println("typed pattern, second form, non capturing"),
+        3, (x) => println("literal pattern"),
+        3, () => println("literal pattern, non capturing"),
+    );
 }
